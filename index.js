af973f9c8a3163c354ab78b2b5448bf9748324ee
yoo7491ap <xyooo7491x@gmail.com>
2020-10-14 09:57:55 +0900

    oh my god

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const token = process.argv.length == 2 ? process.env.token : "";
 const welcomeChannelName = "입퇴장로그";
 const byeChannelName = "입퇴장로그";
 const welcomeChannelComment = "잘와따";
-const byeChannelComment = "잘가.";
+const byeChannelComment = "잘가";
 
 client.on('ready', () => {
   console.log('켰다.');
@@ -64,16 +64,17 @@ client.on('message', (message) => {
     return message.reply('너두 :>');
   }
 
+
   if(message.content == '유앱아 도와줘') {
     let img = 'https://cdn.discordapp.com/avatars/765031566127923220/4b9a15ac349c539b8dd5ccfdbb77d202.png?size=128';
     let embed = new Discord.RichEmbed()
-      .setTitle('유앱보오옷')
-      .setURL('https://twitter.com/yoo7491_ap')
-      .setAuthor('머 도와주까', img, 'https://discord.gg/7kR8tvT')
+      .setTitle('타이틀')
+      .setURL('https://discord.gg/7kR8tvT')
+      .setAuthor('유앱봇', img, 'https://discord.gg/7kR8tvT')
       .setThumbnail(img)
       .addBlankField()
       .addField('자동역할부여', '유저라는 역할 만들면 내가 유저 역할 줄거야')
-      .addField('환영기능', '헤엘로오오', true)
+      .addField('환영기능', '입퇴장로그라는 채널을 만들어바', true)
       .addField('공지기능', '!전체공지', true)
       .addField('대화기능', '유앱아', true)
       .addField('청소기능', '유앱아 청소\n숫자\n쓱싹쓱싹\n')
@@ -86,15 +87,17 @@ client.on('message', (message) => {
     let helpImg = 'https://cdn.discordapp.com/avatars/765031566127923220/4b9a15ac349c539b8dd5ccfdbb77d202.png?size=128';
     let commandList = [
       {name: '유앱아 도와줘', desc: '유앱아 도움'},
-      {name: '유앱아 도움', desc: '머가 궁금한데'},
-      {name: '유앱아 도와줘', desc: '내가 도와주께'},
+      {name: '흐으음', desc: '흐으음'},
+      {name: '유앱아 도와줘', desc: '유앱이 도움줌'},
       {name: '!전체공지', desc: 'dm으로 전체 공지 보내기'},
+      {name: '!전체공지2', desc: 'dm으로 전체 embed 형식으로 공지 보내기'},
       {name: '유앱아 청소', desc: '청소!!!!!!!'},
-      {name: '!초대코드', desc: '초대 코드 표기'},
+      {name: '!초대코드', desc: '해당 채널의 초대 코드 표기'},
+      {name: '!초대코드2', desc: '봇이 들어가있는 모든 채널의 초대 코드 표기'},
     ];
     let commandStr = '';
     let embed = new Discord.RichEmbed()
-      .setAuthor('Help of 유앱봇', helpImg)
+      .setAuthor('유앱이가 도와줄께에에에ㅔ', helpImg)
       .setColor('#186de6')
       .setFooter(`유앱봇`)
       .setTimestamp()
@@ -106,14 +109,53 @@ client.on('message', (message) => {
     embed.addField('Commands: ', commandStr);
 
     message.channel.send(embed)
+  } else if(message.content == '!초대코드2') {
+    client.guilds.array().forEach(x => {
+      x.channels.find(x => x.type == 'text').createInvite({maxAge: 0}) // maxAge: 0은 무한이라는 의미, maxAge부분을 지우면 24시간으로 설정됨
+        .then(invite => {
+          message.channel.send(invite.url)
+        })
+        .catch((err) => {
+          if(err.code == 50013) {
+            message.channel.send('**'+x.channels.find(x => x.type == 'text').guild.name+'** 채널 권한이 없자나')
+          }
+        })
+    });
   } else if(message.content == '!초대코드') {
+    if(message.channel.type == 'dm') {
+      return message.reply('dm에서 사용 못하는데');
+    }
     message.guild.channels.get(message.channel.id).createInvite({maxAge: 0}) // maxAge: 0은 무한이라는 의미, maxAge부분을 지우면 24시간으로 설정됨
       .then(invite => {
         message.channel.send(invite.url)
+      })
+      .catch((err) => {
+        if(err.code == 50013) {
+          message.channel.send('**'+message.guild.channels.get(message.channel.id).guild.name+'** 채널 권한이 없자나')
+        }
+      })
+  } else if(message.content.startsWith('!전체공지2')) {
+    if(checkPermission(message)) return
+    if(message.member != null) { // 채널에서 공지 쓸 때
+      let contents = message.content.slice('!전체공지2'.length);
+      let embed = new Discord.RichEmbed()
+        .setAuthor('유앱이가 도와줄께에에에ㅔㅇ')
+        .setColor('#186de6')
+        .setFooter(`유앱봇`)
+        .setTimestamp()
+  
+      embed.addField('공지: ', contents);
+  
+      message.member.guild.members.array().forEach(x => {
+        if(x.user.bot) return;
+        x.user.send(embed)
       });
-  }
-
-  if(message.content.startsWith('!전체공지')) {
+  
+      return message.reply('공지 전송해써 잘해찌?');
+    } else {
+      return message.reply('채널에서 실행해');
+    }
+  } else if(message.content.startsWith('!전체공지')) {
     if(checkPermission(message)) return
     if(message.member != null) { // 채널에서 공지 쓸 때
       let contents = message.content.slice('!전체공지'.length);
@@ -122,20 +164,22 @@ client.on('message', (message) => {
         x.user.send(`<@${message.author.id}> ${contents}`);
       });
   
-      return message.reply('공지 전송해써 잘해찌');
+      return message.reply('공지 전송해써 잘해찌?');
     } else {
-      return message.reply('채널에서 실행 ㄱ!');
+      return message.reply('채널에서 실행하란마랴');
     }
-  }
+  } else if(message.content.startsWith('!청소')) {
+    if(message.channel.type == 'dm') {
+      return message.reply('dm에서 사용 못해');
+    }
+    
+    if(message.channel.type != 'dm' && checkPermission(message)) return
 
-  if(message.content.startsWith('유앱아 청소')) {
-    if(checkPermission(message)) return
-
-    var clearLine = message.content.slice('유앱아 청소 '.length);
+    var clearLine = message.content.slice('!청소 '.length);
     var isNum = !isNaN(clearLine)
 
     if(isNum && (clearLine <= 0 || 100 < clearLine)) {
-      message.channel.send("1부터 100까지만 입력해")
+      message.channel.send("1부터 100까지만 입력해 선넘지말구")
       return;
     } else if(!isNum) { // c @나긋해 3
       if(message.content.split('<@').length == 2) {
@@ -143,10 +187,9 @@ client.on('message', (message) => {
 
         var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
         var count = parseInt(message.content.split(' ')[2])+1;
-        const _limit = 10;
         let _cnt = 0;
 
-        message.channel.fetchMessages({limit: _limit}).then(collected => {
+        message.channel.fetchMessages().then(collected => {
           collected.every(msg => {
             if(msg.author.id == user) {
               msg.delete();
@@ -159,7 +202,7 @@ client.on('message', (message) => {
     } else {
       message.channel.bulkDelete(parseInt(clearLine)+1)
         .then(() => {
-          AutoMsgDelete(message, `<@${message.author.id}> ` + parseInt(clearLine) + "개의 메시지 지웠다 (이 메세지 곧 소멸됨)");
+          AutoMsgDelete(message, `<@${message.author.id}> ` + parseInt(clearLine) + "개의 메시지 소멸시켜따 (이 메세지도 곧 소멸됨)");
         })
         .catch(console.error)
     }
