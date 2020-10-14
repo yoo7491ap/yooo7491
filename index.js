0bbb2746c1e85976f1c6795a0ee1ee0e99ec15bb
yoo7491ap <xyooo7491x@gmail.com>
2020-10-14 10:37:26 +0900

    ㅇㅁㄴ

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ client.on('message', (message) => {
   if(message.content == '유앱이 멍청이') {
     return message.reply('너두 :>');
   }
-  if(message.content == '서버상태') {
+  if(message.content == '-서버상태') {
     let embed = new Discord.RichEmbed()
     let img = 'https://cdn.discordapp.com/avatars/765031566127923220/4b9a15ac349c539b8dd5ccfdbb77d202.png?size=128';
     var duration = moment.duration(client.uptime).format(" D [일], H [시간], m [분], s [초]");
