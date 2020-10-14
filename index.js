848d8a764262e80392813ab2f1922eef39c021e9
yoo7491ap <xyooo7491x@gmail.com>
2020-10-14 12:02:27 +0900

    ㄴㅇㅈ

--- index.js
+++ index.js
@@ -78,6 +78,10 @@ client.on('message', (message) => {
     return message.reply('안뇽!!!!');
   }
 
+  if(message.content == '유앱아 섻') {
+    return message.reply('으아...역겨워..');
+  }
+
   if(message.content == '-서버상태') {
     let embed = new Discord.RichEmbed()
     let img = 'https://cdn.discordapp.com/avatars/765031566127923220/4b9a15ac349c539b8dd5ccfdbb77d202.png?size=128';
