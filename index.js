f4b6db720e8992f6ca4d73e05dc3cdbc828514d1
yoo7491ap <xyooo7491x@gmail.com>
2021-03-04 11:53:22 +0900

    ㅇㅈㅇㅈ

--- index.js
+++ index.js
@@ -221,6 +221,10 @@ client.on('message', (message) => {
     return message.reply('이 ^^l');
   }
 
+  if(message.content == '유앱 깨못하네') {
+    return message.reply('느그 엄론즈 ㅅㄱ');
+  }
+
   if(message.content == '-서버상태') {
     let embed = new Discord.RichEmbed()
     let img = 'https://cdn.discordapp.com/avatars/765031566127923220/4b9a15ac349c539b8dd5ccfdbb77d202.png?size=128';
